d6f2240c60d8dd6d4f57034bb13619b8f3629109
Remco Bloemen <remco@wicked.ventures>
2022-04-11 04:21:40 +0200

    tweak

--- src/FixedPointMathLib.sol
+++ src/FixedPointMathLib.sol
@@ -57,17 +57,17 @@ library FixedPointMathLib {
     // Integer log2
     // @returns floor(log2(x)) if x is nonzero, otherwise 0. This is the same
     //          as the location of the highest set bit.
-    // Consumes 271 gas. This could have been an 3 gas EVM opcode though.
+    // Consumes 232 gas. This could have been an 3 gas EVM opcode though.
     function ilog2(uint256 x) internal returns (uint256 r) {
         assembly {
-            r := shl(7, gt(x, 0xffffffffffffffffffffffffffffffff))
-            r := or(r, shl(6, gt(shr(r, x), 0xffffffffffffffff)))
-            r := or(r, shl(5, gt(shr(r, x), 0xffffffff)))
-            r := or(r, shl(4, gt(shr(r, x), 0xffff)))
-            r := or(r, shl(3, gt(shr(r, x), 0xff)))
-            r := or(r, shl(2, gt(shr(r, x), 0xf)))
-            r := or(r, shl(1, gt(shr(r, x), 0x3)))
-            r := or(r, gt(shr(r, x), 0x1))
+            r := shl(7, lt(0xffffffffffffffffffffffffffffffff, x))
+            r := or(r, shl(6, lt(0xffffffffffffffff, shr(r, x))))
+            r := or(r, shl(5, lt(0xffffffff, shr(r, x))))
+            r := or(r, shl(4, lt(0xffff, shr(r, x))))
+            r := or(r, shl(3, lt(0xff, shr(r, x))))
+            r := or(r, shl(2, lt(0xf, shr(r, x))))
+            r := or(r, shl(1, lt(0x3, shr(r, x))))
+            r := or(r, lt(0x1, shr(r, x)))
         }
     }
 
